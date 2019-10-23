815446b5eb4eb6ee3055c9923973495a1788b468
zhenlove <121910347@qq.com>
2019-10-23 16:10:25 +0800

    添加工程

---
+++ Example/KMAgoraRtc/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KMAgoraRtc
+//
+//  Created by zhenlove on 10/23/2019.
+//  Copyright (c) 2019 zhenlove. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
